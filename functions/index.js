bceda49b3438a291698d0363c31c2cf7573a63bd
aakashbista <Bistaakash123@gmail.com>
2020-06-01 09:10:19 +1000

    update endpoint created

--- functions/index.js
+++ functions/index.js
@@ -1,108 +1,78 @@
+import { https } from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { initializeApp, database as _database } from "firebase-admin";
 
-const functions = require("firebase-functions");
-const cors = require('cors')({ origin: true });
-const admin = require('firebase-admin');
-
-admin.initializeApp();
-
-const database = admin.database().ref('/notes');
-
-exports.addNotes = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'POST') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            })
-        }
-        const {
-            address,
-            description,
-            imageUrl,
-            lat,
-            lon,
-            userId
-        } = req.body;
-
-        database.push(req.body);
-        res.status(200).json({ message: `Added successFully` })
-    }, (error) => {
-        res.status(error.code).json({
-            message: `Something went wrong. ${error.message}`
-        })
-    })
-})
+initializeApp();
+const database = _database().ref('/notes');
 
 
-exports.getNotes = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'GET') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            })
-        }
+const app = express();
+app.use(cors({ origin: true }))
 
-        let notes = [];
+app.get("/", (req, res) => {
 
-        return database.on('value', (snapshot) => {
-            snapshot.forEach((item) => {
-                notes.push({
-                    id: item.key,
-                    address: item.val().address,
-                    description: item.val().description,
-                    imageUrl: item.val().imageUrl,
-                    lat: item.val().lat,
-                    lon: item.val().lon,
-                    userId: item.val().userId,
+    let notes = [];
 
-                });
+    return database.on('value', (snapshot) => {
+        snapshot.forEach((item) => {
+            notes.push({
+                id: item.key,
+                address: item.val().address,
+                description: item.val().description,
+                imageUrl: item.val().imageUrl,
+                lat: item.val().lat,
+                lon: item.val().lon,
+                userId: item.val().userId,
 
             });
 
-            res.status(200).json(notes)
-        }, (error) => {
-            res.status(error.code).json({
-                message: `Something went wrong. ${error.message}`
-            })
-        }
-        )
-    }
-    )
-})
-exports.getNotesById = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'GET') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            })
-        }
-        const id = req.query.id
-        let dbNoteRef = database.child(`/${id}`);
-        return dbNoteRef.once('value', (snapshot) => {
-            let note = snapshot.val();
-            res.status(200).json(note)
-        }, (error) => {
-            res.status(error.code).json({
-                message: `Something went wrong. ${error.message}`
-            })
-        }
-        )
-    }
-    )
-})
+        });
 
-exports.delete = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'DELETE') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            })
-        }
-        const id = req.query.id
-        database.child(`/${id}`).remove();
-        res.status(200).json({ message: `Added Deleted` })
+        res.status(200).json(notes)
     }, (error) => {
         res.status(error.code).json({
             message: `Something went wrong. ${error.message}`
         })
+    }
+    )
+})
+
+app.post("/", async (req, res) => {
+    const notes = req.body;
+    await database.push(notes);
+    res.status(200).json({ message: `Added successFully` })
+}, (error) => {
+    res.status(error.code).json({
+        message: `Something went wrong. ${error.message}`
     })
 })
+
+
+app.put("/:id", async (req, res) => {
+    const body = req.body;
+    await database.child(req.params.id).update(body);
+
+    res.status(200).send()
+});
+
+app.get("/:id", async (req, res) => {
+    let dbNoteRef = database.child(req.params.id);
+    return await dbNoteRef.once('value', (snapshot) => {
+        let note = snapshot.val();
+        res.status(200).json(note)
+    }, (error) => {
+        res.status(error.code).json({
+            message: `Something went wrong. ${error.message}`
+        })
+    }
+    )
+})
+
+app.delete("/:id", async (req, res) => {
+    await database.child(req.params.id).remove();
+    res.status(200).json({ message: `Added Deleted` })
+})
+
+
+export const notes = https.onRequest(app);
